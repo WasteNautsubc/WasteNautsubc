a062d3dc483daa2b3a23cae74142a7f3e8d44a3d
WasteNautsubc <admin@wastenautsubc.com>
2024-01-30 23:01:41 -0800

    ...

--- src/components/TWRSection.js
+++ src/components/TWRSection.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './TWRSection.css';
 
 const TWRSection = () => {
   return (
     <div className='TWR-container'>
-      <a href="/textile-waste-recycling" className='TextileWaste'>
+     <Link to="/textile-waste-recycling" className='TextileWaste'>
        <img src="./images/TWR.png" alt="TWR logo"/>
-       </a>
+       </Link>
       <p1> CURRENT PROJECTS (2023-24 DESIGN CYCLE)</p1>
-      <a href="/textile-waste-recycling" className='TextileWaste'>
-      <p2>TEXTILE WASTE RECYCLING</p2></a>
-      
+      <Link to="/textile-waste-recycling" className='TextileWaste'>
+      <p2>TEXTILE WASTE RECYCLING</p2>
+      </Link>
       <p3>The goal of this project is to find a way to create value from textile waste and educate</p3>
         <p4>other students on sustainable fashion practices.</p4>
     
